5defeb02084686043ff831d1444000186a0dbbac
irisluo10 <irisluo@unc.edu>
2024-10-15 16:30:37 -0400

    lessons folder created and lesson activity

---
+++ lessons/unit_tests/list_fns.py
@@ -0,0 +1,11 @@
+def get_first(list):
+    return list[0]
+
+
+def remove_first(list):
+    list.pop(0)
+
+
+def get_and_remove_first(list):
+    item = list.pop(0)
+    return item
---
+++ lessons/unit_tests/list_fns_test.py
@@ -0,0 +1,19 @@
+from lessons.unit_tests.list_fns import get_first, remove_first, get_and_remove_first
+
+
+def test_get_first() -> None:
+    fruits: list[str] = ["apples", "oranges", "bananas"]
+    # checks that return value is the same as assertion
+    assert get_first(fruits) == "apples"
+
+
+def test_remove_get_first() -> None:
+    fruits: list[str] = ["apples", "oranges", "bananas"]
+    # checks that return value is None
+    assert remove_first(fruits) is None
+
+
+def test_remove_first_behavior() -> None:
+    fruits: list[str] = ["apples", "oranges", "bananas"]
+    remove_first(fruits)
+    assert fruits == ["oranges", "bananas"]
